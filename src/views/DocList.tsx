5bf2d57b086bea74ff7a973c9f409fbd8fbc7088
onlovelamp <patrickbagtas@gmail.com>
2024-02-07 11:06:16 -0800

    new document
    -info

--- src/views/DocList.tsx
+++ src/views/DocList.tsx
@@ -3,6 +3,9 @@
 export default function DocList(){
     return (
         <div>
+            <div className="topper">
+                <p>+ Create Document</p>
+            </div>
             <div className="box ">
                 <p>box</p>
             </div>
