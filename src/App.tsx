c8df23c8238d86ffaafe8043b12aea02718b3176
onlovelamp <patrickbagtas@gmail.com>
2024-03-15 11:23:49 -0700

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import { Routes,Navigate,Route,Link } from 'react-router-dom'
 import CreateReg from './views/CreateReg'
 import LoginReg from './views/LoginReg'
 import DocList from './views/DocList'
-import './App.css'
+import Header from './components/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='app'>
-      <div className='navbar'>
-        <Link to ={`/doclist`}><p>Doc</p></Link>
-      </div>
+      <Header></Header>
       <div className='container'>
-        <h1>PDF Scanner</h1>
         <Routes>
           <Route path="/" element={<Navigate to="register"/>}/>
           <Route path="/register" element={<CreateReg/>}/>
