d3bc844f0caf4b5c973f1aa19bb87cc88a928bde
Patrick Bagtas <122561605+PatrickBagtas@users.noreply.github.com>
2024-07-17 16:05:36 -0700

    Revert "With style"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,11 @@
-import  { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom"
 
-export default function Header() {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
+export default function Header(){
 
-    return (
+    return(
         <div className='navbar'>
-<<<<<<< HEAD
-                <h1>PDF Scanner</h1>
-                
+        <Link to ={`/doclist`}><p>Doc</p></Link>
     </div>
     )
-=======
-            <ul>
-                <li><Link to="/Login">PDF Scanner</Link></li>
-                <li><Link to="/doclist">Doc</Link></li>
-                <li
-                    className={`list-item dropdown ${isDropdownOpen ? 'open' : ''}`}
-                    onMouseEnter={toggleDropdown}
-                    onMouseLeave={toggleDropdown}
-                >
-                    <span className="dropdown-toggle">Dropdown</span>
-                    <ul className="dropdown-menu">
-                        <li className="dropdown-item"><Link to="/get-started">Get Started</Link></li>
-                        <li className="dropdown-item"><Link to="/community">Community</Link></li>
-                        <li className="dropdown-item"><Link to="/general-questions">General Questions</Link></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    );
->>>>>>> c8df23c8238d86ffaafe8043b12aea02718b3176
 }
--- src/views/DocList.tsx
+++ src/views/DocList.tsx
@@ -3,21 +3,9 @@
 export default function DocList(){
     return (
         <div>
-<<<<<<< HEAD
-            <div className="scanbutton">
-                <p>Drag file in the box!</p>
-                <button className="scan"> Scan</button>
-=======
-            <div className="topper">
-                <p>+ Create Document</p>
-            </div>
             <div className="box ">
                 <p>box</p>
->>>>>>> c8df23c8238d86ffaafe8043b12aea02718b3176
             </div>
-                <div className="box ">
-                    <p>+</p>
-                </div>
         </div>
     )
 }
